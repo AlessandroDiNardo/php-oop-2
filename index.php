5410d2cd16c5ee4e53094e060e3a660189cc2188
AlessandroDiNardo <alexdna003@gmail.com>
2023-01-25 18:11:40 +0100

    problem to show on screen

--- index.php
+++ index.php
@@ -13,14 +13,108 @@
     ?> 
 </head>
 <body>
-    <h1>PET SHOP</h1>
+    <div class="container">
+        <h1>PET SHOP</h1>
 
-    <h2>PRODOTTI:</h2>
+        <h2>PRODOTTI:</h2>
 
-    <h3>CIBO</h3>
+        <div class="cards-container">
+            <h3>CIBO</h3>   
+            <div class="card">
+                <?php 
+                    foreach ($foods as $food) { 
+                        ?>
 
-    <h3>CUCCE</h3>
+                        <h5 class="card-title">
+                            <?php echo $food -> name ?>
+                        </h5>
+                        <p class="card-text">
+                            <?php echo $food -> description ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $prodotto -> price ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $food -> brand ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $food -> quantity  ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $food -> category ' $' ?>
+                        </p>
 
-    <h3>GIOCHI</h3>
+                        <?php 
+                    } 
+                ?>
+            </div>
+        </div>
+        <div class="cards-container">
+            <h3>CUCCE</h3>   
+            <div class="card">
+                <?php 
+                    foreach ($  kennels as $kennel) { 
+                        ?>
+
+                        <h5 class="card-title">
+                            <?php echo $kennel -> name ?>
+                        </h5>
+                        <p class="card-text">
+                            <?php echo $kennel -> description ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $prodotto -> price ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $kennel -> brand ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $kennel -> dimension  ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $kennel -> material  ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $kennel -> category ' $' ?>
+                        </p>
+
+                        <?php 
+                    } 
+                ?>
+            </div>
+        </div>
+
+        <div class="cards-container">
+            <h3>GIOCHI</h3>   
+            <div class="card">
+                <?php 
+                    foreach ($games as $game) { 
+                        ?>
+
+                        <h5 class="card-title">
+                            <?php echo $game -> name ?>
+                        </h5>
+                        <p class="card-text">
+                            <?php echo $game -> description ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $prodotto -> price ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $game -> brand ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $game -> dimension  ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $game -> category ' $' ?>
+                        </p>
+
+                        <?php 
+                    } 
+                ?>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
